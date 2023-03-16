1d5cfe5479150c3ac8fe3a8193b7a8bf8879a8f1
AndersonLe17 <anderleorellana@gmail.com>
2023-03-16 13:17:48 -0600

    Design Login base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './styles/App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/login"} element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
